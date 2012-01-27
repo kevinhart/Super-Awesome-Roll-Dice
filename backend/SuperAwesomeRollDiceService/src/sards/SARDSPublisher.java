360d699490f6f9b54cbec4f40dc2f50f973e8e79
Kevin Hart <kevin@k-hart.com>
2012-01-27 16:28:55 -0500

    Parameterized SARDSPublisher endpoint.

--- backend/SuperAwesomeRollDiceService/src/sards/SARDSPublisher.java
+++ backend/SuperAwesomeRollDiceService/src/sards/SARDSPublisher.java
@@ -4,6 +4,10 @@ import javax.xml.ws.Endpoint;
 
 public class SARDSPublisher {
 	public static void main( String[] args ) {
-		Endpoint.publish( "http://kubrick.student.rit.edu:8080/SARDS", new SARDS() );
+		if ( args.length < 1 ) {
+			System.err.println( "usage: SARDSPublisher ENDPOINT_ADDRESS" );
+		} else {
+			Endpoint.publish( args[ 0 ], new SARDS() );
+		}
 	}
 }
