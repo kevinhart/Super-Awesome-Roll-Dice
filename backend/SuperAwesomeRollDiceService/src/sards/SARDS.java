b3b6b365b4a7102bcdaacb9a06101b522c44ad4c
thetalkingdude <rhs4656@g.rit.edu>
2012-02-19 20:22:51 -0500

    Fixed [] to {}

--- backend/SuperAwesomeRollDiceService/src/sards/SARDS.java
+++ backend/SuperAwesomeRollDiceService/src/sards/SARDS.java
@@ -191,7 +191,7 @@ public class SARDS implements Provider< Source > {
 		Xquery.append(" return <names>{data($x/../@name)}:{data($x/@name)} </names>");
 		Iterator it = doQuery(Xquery.toString());
 		String curUsername = "";
-		resultBuilder.append("[");
+		resultBuilder.append("{}");
 		while(it.hasNext()){
 				XdmItem item = (XdmItem)it.next();
 				String aString = item.toString();
@@ -209,8 +209,12 @@ public class SARDS implements Provider< Source > {
 				}
 					resultBuilder.append("\""+tmp[1]+"\", ");
 			}
+			if(resultBuilder.length() > 5){
 			resultBuilder.delete(resultBuilder.length()-2,resultBuilder.length());
-			resultBuilder.append("]]");
+			resultBuilder.append("]}");
+			}else{
+			resultBuilder.append("}");
+			}
 			String result = resultBuilder.toString();
 		return "{\"r\":0,\"t\":\"[xQueryStatement] Success.\",\"d\":\"" + result + "\"}";
 	}
